cab152570392485907eb729012db4b8fc2af50f3
Metin Ahmedov <129603823+metotg@users.noreply.github.com>
2023-11-22 22:03:05 +0200

    SelectColor

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class Player {
     // color of Pieces
-    //Paying turn
+    Color color;
+    Scanner sc = new Scanner(System.in);
+    public void selectColor() {
+        boolean validInput = false;
+        do {
+            System.out.println("Select your color of pieces BLACK/WHITE:");
+            try {
+                color = Color.valueOf(sc.nextLine().toUpperCase());
+                validInput = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid color. Please choose a valid color.");
+            }
+        } while (!validInput);
+    }
+
+    public void closeScanner() {
+        sc.close();
+    }
 }
