be078252f5218705f5aae6faf612955e2f0991f6
Metin Ahmedov <129603823+metotg@users.noreply.github.com>
2023-11-22 22:28:47 +0200

    Pieces

--- src/Piece.java
+++ src/Piece.java
@@ -32,7 +32,7 @@ public class Piece {
 
     public boolean isValid(Board board, int fromX, int fromY, int toX, int toY){
         if(toX == fromX && toY == fromY)
-            return false; //cannot move nothing
+            return false;
         if(toX < 0 || toX > 7 || fromX < 0 || fromX > 7 || toY < 0 || toY > 7 || fromY <0 || fromY > 7)
             return false;
         return true;
---
+++ src/Queen.java
@@ -0,0 +1,22 @@
+public class Queen extends Piece{
+
+    public Queen(boolean available, int x, int y) {
+        super(available, x, y);
+    }
+
+    @Override
+    public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
+        if(super.isValid(board, fromX, fromY, toX, toY) == false)
+            return false;
+        //diagonal
+        if(toX - fromX == toY - fromY)
+            return true;
+        if(toX == fromX)
+            return true;
+        if(toY == fromY)
+            return true;
+
+        return false;
+    }
+
+}
